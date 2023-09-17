0394bb2cdcf728fee7629855ed1026f44d714ecc
Emmanuel Gautier <git@emmanuelgautier.fr>
2023-09-17 16:28:23 +0200

    chore: make contentlayer config simpler

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,55 +1,23 @@
 import {
-  defineDocumentType,
-  makeSource,
-} from 'contentlayer/source-files'
-import {
-  ContentLayerArticleFields,
-  ContentLayerWebsiteFields,
-  ContentLayerWebPageElementFields,
-  ContentLayerOrganizationFields,
-  ContentLayerPageFields,
-  ContentLayerPersonFields
-} from '@galactiks/explorer'
-
-const Article = defineDocumentType(() => ({
-  ...ContentLayerArticleFields,
-  filePathPattern: 'articles/**/*.mdx',
-  contentType: 'mdx',
-}))
-
-const Page = defineDocumentType(() => ({
-  ...ContentLayerPageFields,
-  filePathPattern: 'pages/**/*.mdx',
-  contentType: 'mdx',
-}))
-
-const Person = defineDocumentType(() => ({
-  ...ContentLayerPersonFields,
-  filePathPattern: 'persons/**/*.mdx',
-  contentType: 'mdx',
-}))
-
-const Organization = defineDocumentType(() => ({
-  ...ContentLayerOrganizationFields,
-  filePathPattern: 'organizations/**/*.mdx',
-  contentType: 'mdx',
-}))
-
-const Website = defineDocumentType(() => ({
-  ...ContentLayerWebsiteFields,
-  filePathPattern: 'websites/*.mdx',
-  contentType: 'mdx',
-}))
-
-const WebpageElement = defineDocumentType(() => ({
-  ...ContentLayerWebPageElementFields,
-  filePathPattern: 'elements/*.mdx',
-  contentType: 'mdx'
-}))
+  ArticleDocumentType,
+  OrganizationDocumentType,
+  PageDocumentType,
+  PersonDocumentType,
+  WebsiteDocumentType,
+  WebpageElementDocumentType,
+} from '@galactiks/contentlayer/configs'
+import { makeSource } from 'contentlayer/source-files';
 
 const contentLayerConfig = makeSource({
   contentDirPath: 'content',
-  documentTypes: [Article, Organization, Page, Person, Website, WebpageElement],
+  documentTypes: [
+    ArticleDocumentType,
+    OrganizationDocumentType,
+    PageDocumentType,
+    PersonDocumentType,
+    WebsiteDocumentType,
+    WebpageElementDocumentType,
+  ],
   mdx: {},
   disableImportAliasWarning: true,
 })
